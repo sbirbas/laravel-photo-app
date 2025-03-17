90f51ee5823b8ac522a61ea2c0c23001a8a96fac
sbirbas <sophia@track5media.com>
2025-03-17 11:46:25 -0400

    pausing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div id="hero">
-    <img id="hero-img" class="img-fluid" src="{{ asset('media/halfmoon.JPG') }}" alt="moon"/>
-
-
+   Coming Soon...
 </div>
-    <div id ="showcase" class="text-center pt-5">Showcase</div>
-@for ($i = 0 ; $i <= 5 ; $i++)
-{{--    @foreach($photos as $photo)--}}
-{{--        {{$photo}}--}}
-{{--    @endforeach--}}
-@endfor
 @endsection
