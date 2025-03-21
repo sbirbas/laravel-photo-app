c455edb57ae937df7e22bdae1ca6fb7c94372f33
sbirbas <sophia@track5media.com>
2025-03-20 23:43:52 -0400

    some little touches

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="hero-content">
-        <h1 class="mt-5">SOPHIA BIRBAS</h1>
+        <h1 class="">SOPHIA BIRBAS</h1>
             <div id="hero">
                 <img id="hero-img" class="pt-1 img fluid" src="{{ asset('media/hero-img.JPG') }}" alt="Leaves"/>
                 <img id="hero-bird" class="d-sm-none d-xs-block pt-1 img fluid" src="{{ asset('media/bird.jpeg') }}" alt="Leaves"/>
